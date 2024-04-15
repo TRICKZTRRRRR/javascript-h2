25249b0c427210b340fd4f02d4f327a10467c273
Jonas Steinmejer <jonas04@outlook.dk>
2024-04-15 10:45:46 +0200

    Added comments

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// Makes an alert, that ays Hello World, when the page is loaded
 function pageLoad() {
     window.addEventListener(alert("Hello World"));
 }
 
+// Changes the value of my paragraph, to Hello World
 function helloWorld() {
     document.getElementById("world").innerHTML = "Hello World";
 }
